d2e0b0b724be32d709d30ea1675df12b9ec642bf
sbkhaloof <sehammalkawi92@gmail.com>
2021-07-12 19:26:24 +0300

    lab12

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
 
   render() {
     console.log('app', this.props);
-    
+    // const {user,isAuthenticated}=this.props.auth0;
     return (
       <>
         <Router>
